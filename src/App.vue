521283ad5a0996c83c07cfeef6f3bd35a136d93c
janny20220322 <janny20220322@staff.1111.com.tw>
2022-07-13 16:42:08 +0800

    202207131642

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/0d532b3f-0196-466a-96e1-7c6db56d0142.gif"/>
-   <img alt="Vue logo" src="./assets/process_pc.3d30d576.gif"/>
-   <img alt="Vue logo" src="./assets/15efe7cafff97b50d17bbc55709ba658.gif"/>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    01
+    <picture>
+        <source media="(min-width: 500px)" srcset="@/assets/0d532b3f-0196-466a-96e1-7c6db56d0142.gif" type="image/jpg">
+        <source media="(max-width: 500px)" srcset="@/assets/process_pc.3d30d576.gif" type="image/jpg">
+        <img class="banner" draggable="false" src="img/m/process_m.gif" alt="活動流程">
+    </picture>
+    02
+    <picture>
+        <source media="(min-width: 500px)" srcset="@/assets/0d532b3f-0196-466a-96e1-7c6db56d0142.gif" type="image/jpg">
+        <source media="(max-width: 500px)" srcset="@/assets/process_pc.3d30d576.gif" type="image/jpg">
+        <img class="banner" draggable="false" src="@/assets/process_pc.3d30d576.gif" alt="活動流程">
+    </picture>
+    <img alt="Vue logo" src="./assets/0d532b3f-0196-466a-96e1-7c6db56d0142.gif" />
+    <img alt="Vue logo" src="./assets/process_pc.3d30d576.gif" />
+    <img alt="Vue logo" src="./assets/15efe7cafff97b50d17bbc55709ba658.gif" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            HelloWorld
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
